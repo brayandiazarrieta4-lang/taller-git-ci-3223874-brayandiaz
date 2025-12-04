9e9e82ef9b218e64b6141bbaf23b3948a726904c
brayan diaz <brayandiazarrieta4@gmail.com>
2025-12-04 08:20:59 -0500

    test: corregir test a CommonJS para node --test

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,5 @@
-import assert from "assert";
-import { suma } from "../src/app.js";
+const assert = require("assert");
+const { suma } = require("../src/app.js");
 
 describe("suma", () => {
   it("suma 2 + 3 = 5", () => {
